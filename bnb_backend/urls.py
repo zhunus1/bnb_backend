af26c9c614a6f2870b4dbb7e7aed4185d232e2bc
zhunus1 <tengri.07@mail.ru>
2025-07-14 11:06:19 +0500

    Fix

--- bnb_backend/urls.py
+++ bnb_backend/urls.py
@@ -38,13 +38,13 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')), 
-    path('modules/', include('modules.urls')),
-    path('profiles/', include('profiles.urls')),
-    path('articles/', include('medias.urls')),
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    re_path(r'^auth/', include('drf_social_oauth2.urls', namespace='drf')),
+    path('api/admin/', admin.site.urls),
+    path('api/users/', include('users.urls')), 
+    path('api/modules/', include('modules.urls')),
+    path('api/profiles/', include('profiles.urls')),
+    path('api/articles/', include('medias.urls')),
+    path('api/swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'api/^auth/', include('drf_social_oauth2.urls', namespace='drf')),
 ]
